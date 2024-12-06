c8c055ea3216f9ebe1d6284e734ed8740cbf63cb
YahyaLem02 <Lemkharbechy@gmail.com>
2024-12-06 14:33:57 +0100

    last update for view

--- App/Views/Home/Layout/Layout.php
+++ App/Views/Home/Layout/Layout.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>e-Stock - Système de Gestion</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="../../public/assets/css/main.css">
+    <link rel="stylesheet" href="/public/assets/css/main.css">
 
 </head>
 <body class="bg-gray-50">
